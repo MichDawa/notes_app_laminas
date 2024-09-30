7a957b6f3c3344d1ccb6899308f8814b12a20cd2
MichDawa <mldawa@yssp.ph>
2024-09-30 14:51:11 +0800

    commit cors setup

--- module/Notes/src/Module.php
+++ module/Notes/src/Module.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Notes;
 
+use Laminas\Mvc\MvcEvent;
 use Laminas\Db\Adapter\Adapter;
 
 class Module
@@ -25,5 +26,31 @@ class Module
                 },
             ],
         ];
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $app = $e->getApplication();
+        $em  = $app->getEventManager();
+
+        $em->attach(MvcEvent::EVENT_ROUTE, [$this, 'enableCors'], -1000);
+    }
+
+    public function enableCors(MvcEvent $e)
+    {
+        /** @var \Laminas\Http\Response $response */
+        $response = $e->getResponse();
+        /** @var \Laminas\Http\Request $request */
+        $request  = $e->getRequest();
+
+        $response->getHeaders()->addHeaderLine('Access-Control-Allow-Origin', '*')
+            ->addHeaderLine('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->addHeaderLine('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        if ($request->getMethod() === 'OPTIONS') {
+            // Return early for preflight requests
+            $response->setStatusCode(200);
+            return $response;
+        }
     }
 }
